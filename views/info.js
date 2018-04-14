de66586152c96d1af4666713ebca5cd339309563
Wilma Eklund <wilmahill03@gmail.com>
2018-04-14 10:49:25 +0200

    Fixed info

--- views/info.js
+++ views/info.js
@@ -1,33 +1,37 @@
 var insert = "";
-$.getJSON( "list", function( datalist ) {
+$.getJSON( "192.168.56.255:3000/list", function( datalist ) {
   for(var i=0;i<10;i++) {
-    $.getJSON("vote?vote="+datalist.voteringar[i].votering_id, function(data) {
+    $.getJSON("192.168.56.255:3000/vote?vote="+datalist.voteringar[i].votering_id, function(data) {
       var totalvotes=0;
       var title="Unknown law";
+      var activevotes;
       if (datalist.hasOwnProperty('title')) {
         totalvotes=datalist.title;
       }
-      insert = insert.concat("<div class=\"subunit\">" + title + " <div class=\"yesvote\">Yes<br />");
+      insert = insert.concat("<div class=\"subunit\"><h4>" + title + " </h4><div class=\"yesvote\">Yes<br />");
       $.each(data.parti_roster.j, function(key,value) {
         totalvotes+=value;
+        activevotes+=value/10;
         insert=insert.concat("<div class=\"partycell\" style=\"background-color:" + getColor(key) + ";width:" + value/10 + "vw;height:1vw\">" + key + "</div>");
       });
       insert = insert.concat("</div>");
       insert = insert.concat("<div class=\"novote\">No<br />");
       $.each(data.parti_roster.n, function(key,value) {
         totalvotes+=value;
+        activevotes+=value/10;
         insert=insert.concat("<div class=\"partycell\" style=\"background-color:" + getColor(key) + ";width:" + value/10 + "vw;height:1vw\">" + key + "</div>");
       });
-      insert = insert.concat("</div></div>");
+      insert = insert.concat("</div>");
       if (data.parti_roster.hasOwnProperty('total_f')) {
         totalvotes+=data.parti_roster.total_f;
       }
       if (data.parti_roster.hasOwnProperty('total_a')) {
         totalvotes+=data.parti_roster.total_a;
       }
-      insert = insert.concat("<div class=\"neutralvote\">Novote " + totalvotes + "<br />");
+      activevotes=(349-activevotes)/10;
+      insert = insert.concat("<div class=\"neutralvote\" style=\"width:"+ activevotes + "vw;\">Novote<br />");
       insert=insert.concat("<div class=\"partycell\" style=\"background-color:" + getColor("-") + ";width:" + (data.parti_roster.total_f+data.parti_roster.total_a)/10 + "vw;height:1vw\">" + "-" + "</div>");
-      insert = insert.concat("</div><br/><br>");
+      insert = insert.concat("</div><br/><br><br></div>");
       console.log(insert);
       document.getElementById("votes").innerHTML = insert;
     });
