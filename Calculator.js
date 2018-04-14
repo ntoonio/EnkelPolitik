67c26e6db392f97216c7faacaf16498229df2fba
electro squid <fdov80@yahoo.se>
2018-04-14 15:07:25 +0200

    Implement Calculator in Backend.js
    i implemented calc.js and its child-methods in backend.js beacuse SOMEONE ELSE did not want to

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,4 @@
+function Calc() {
 var l= Votes(10,4,1,1);
 	var OP=prompt("what is your opinion on this draft");
 	var IMP_STRING=prompt("how impotant do you consider this draft to be");
@@ -33,6 +34,7 @@ for(var k=0; k<=Ledamöter_Röster.length-1; k++) {
 for(var i=1; i<=Parties.length-1; i++) {
 	Parties[i]= MumsKorv(DIN_OP,Votes(Ledamöter[i],sum,SumOfYes,SumOfNo),IMP);
     
+}
 }
 }
 function Votes( ledamöter,  platser,  ja,  nej) {
--- backend.js
+++ backend.js
@@ -121,3 +121,53 @@ app.get("/vote", function (req, res) {
 app.listen(3000, function() {
 	console.log("Running!")
 })
+function Calc() {
+var l= Votes(10,4,1,1);
+	var OP=prompt("what is your opinion on this draft");
+	var IMP_STRING=prompt("how impotant do you consider this draft to be");
+
+	var DIN_OP=parseFloat(OP);
+	var IMP=parseFloat(IMP_STRING);
+	var PO=1;
+
+var Parties[0,0,0,0,0,0,0,0,0];
+var Ledamöter[0,0,0,0,0,0,0,0,0,0,0,0];
+var Ledamöter_Röster[0,0,0,0,0,0,0,0,0,0,0,0,0,][0,0,0,0,0,0,0,0,0,0,0,0,0,];
+var sum = 0;
+var SumOfYes=0;
+var SumOfNo=0;
+for(var j=0; j<=Ledamöter.length-1; j++) {
+	
+	sum+=Ledamöter[j];
+}
+for(var k=0; k<=Ledamöter_Röster.length-1; k++) {
+	for(var m=0; l<=Ledamöter_Röster.length-1; l++) {
+	if(Ledamöter_Röster[k][l]==1) {
+		
+		SumOfYes++;
+	}
+	else if(Ledamöter_Röster[k][l]==0) {
+		
+		SumOfNo++;
+	}
+	}
+}
+
+
+for(var i=1; i<=Parties.length-1; i++) {
+	Parties[i]= MumsKorv(DIN_OP,Votes(Ledamöter[i],sum,SumOfYes,SumOfNo),IMP);
+    
+}
+}
+}
+function Votes( ledamöter,  platser,  ja,  nej) {
+	return platser/(ledamöter-nej);
+}
+
+function MumsKorv( d,  p,  v) {
+		return d*p*v;
+	}
+
+
+
+}
