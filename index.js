13d8116532e4d6485fc143fdaa90c074e413675e
Anton Lindroth <ntoonio@gmail.com>
2018-04-14 20:57:54 +0200

    Made getQuizStats ready

--- index.js
+++ index.js
@@ -8,53 +8,6 @@ var app = express()
 
 app.use("/views", express.static(path.join(__dirname, "views")))
 app.use("/views/photos", express.static(path.join(__dirname, "views")))
-///////// 
-
-
-
-
-function calc(parties, members, memberVotes) {
-	var opinion = parseFloat(prompt("what is your opinion on this draft"))
-	var importance = parseFloat(prompt("how impotant do you consider this draft to be"))
-
-	var sum = 0
-	var sumOfYes = 0
-	var sumOfNo = 0
-
-	for (var j = 0; j <= members.length - 1; j++) {
-		sum += members[j]
-	}
-
-	for (var k= 0; k <= memberVotes.length - 1; k++) {
-		for (var l = 0; l <= memberVotes.length - 1; l++) {
-			if (memberVotes[k][l] == 1) {
-				sumOfYes++
-			}
-			else if (memberVotes[k][l] == 0) {
-				SumOfNo++
-			}
-		}
-	}
-
-	for (var i = 1; i <= parties.length - 1; i++) {
-		const p = members[i] / (sum - sumOfNo)
-		parties[i] = opinion * p * importance
-	}
-
-	return parties
-}
-
-var parties = []
-var memebers = []
-var memberVotes = []
-
-//console.log(calc(parties, members, memberVotes))
-
-
-
-////////////////
-
-
 
 function request(url, response) {
 	http.get(url, (res) => {
@@ -160,6 +113,43 @@ function getVote(id, response) {
 app.get("/getQuiz", function (req, res) {
 	res.setHeader('Content-Type', 'application/json');
 	res.sendFile("quiz.json", {root: __dirname });
+})
+
+app.get("/getQuizStats", function (req, res) {
+	var sum = 0
+	var sumOfYes = 0
+	var sumOfNo = 0
+
+	for (var j = 0; j <= members.length - 1; j++) {
+		sum += members[j]
+	}
+
+	for (var k= 0; k <= memberVotes.length - 1; k++) {
+		for (var l = 0; l <= memberVotes.length - 1; l++) {
+			if (memberVotes[k][l] == 1) {
+				sumOfYes++
+			}
+			else if (memberVotes[k][l] == 0) {
+				SumOfNo++
+			}
+		}
+	}
+
+	for (var i = 1; i <= parties.length - 1; i++) {
+		const p = members[i] / (sum - sumOfNo)
+		parties[i] = opinion * p * importance
+	}
+	
+	var opinion = parseFloat(req.query.opinion)
+	var importance = parseFloat(req.query.importance)
+	
+	// TODO: Hitta data för dessa variabler
+	// Jag vet inte hur det här fungerar så någon anna får fixa
+	var parties = []
+	var memebers = []
+	var memberVotes = []
+
+	res.send(JSON.stringify(parties))
 })
 
 app.get("/vote", function (req, res) {
