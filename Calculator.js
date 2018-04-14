f31a67d44bba7716b5deca6fc942996907602010
electro squid <fdov80@yahoo.se>
2018-04-14 18:59:37 +0200

    Added Antons edit of Calculator,js
    A clean, bug free version of calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,50 +1,36 @@
-function Calc() {
-var l= Votes(10,4,1,1);
-	var OP=prompt("what is your opinion on this draft");
-	var IMP_STRING=prompt("how impotant do you consider this draft to be");
+function calc(parties, members, memberVotes) {
+	var opinion = parseFloat(prompt("what is your opinion on this draft"))
+	var importance = parseFloat(prompt("how impotant do you consider this draft to be"))
 
-	var DIN_OP=parseFloat(OP);
-	var IMP=parseFloat(IMP_STRING);
-	var PO=1;
+	var sum = 0
+	var sumOfYes = 0
+	var sumOfNo = 0
 
-var Parties[0,0,0,0,0,0,0,0,0];
-var Ledamöter[0,0,0,0,0,0,0,0,0,0,0,0];
-var Ledamöter_Röster[0,0,0,0,0,0,0,0,0,0,0,0,0,][0,0,0,0,0,0,0,0,0,0,0,0,0,];
-var sum = 0;
-var SumOfYes=0;
-var SumOfNo=0;
-for(var j=0; j<=Ledamöter.length-1; j++) {
-	
-	sum+=Ledamöter[j];
-}
-for(var k=0; k<=Ledamöter_Röster.length-1; k++) {
-	for(var m=0; l<=Ledamöter_Röster.length-1; l++) {
-	if(Ledamöter_Röster[k][l]==1) {
-		
-		SumOfYes++;
-	}
-	else if(Ledamöter_Röster[k][l]==0) {
-		
-		SumOfNo++;
-	}
-	}
-}
-
-
-for(var i=1; i<=Parties.length-1; i++) {
-	Parties[i]= MumsKorv(DIN_OP,Votes(Ledamöter[i],sum,SumOfYes,SumOfNo),IMP);
-    
-}
-}
-}
-function Votes( ledamöter,  platser,  ja,  nej) {
-	return platser/(ledamöter-nej);
-}
-
-function MumsKorv( d,  p,  v) {
-		return d*p*v;
+	for (var j = 0; j <= members.length - 1; j++) {
+		sum += members[j]
 	}
 
+	for (var k= 0; k <= memberVotes.length - 1; k++) {
+		for (var l = 0; l <= memberVotes.length - 1; l++) {
+			if (memberVotes[k][l] == 1) {
+				sumOfYes++
+			}
+			else if (memberVotes[k][l] == 0) {
+				SumOfNo++
+			}
+		}
+	}
 
+	for (var i = 1; i <= parties.length - 1; i++) {
+		const p = members[i] / (sum - sumOfNo)
+		parties[i] = opinion * p * importance
+	}
 
+	return parties
 }
+
+var parties = 		[]
+var memebers = 		[]
+var memberVotes = 	[]
+
+console.log(calc(parties, members, memberVotes))
